4080d93b2f109d376d7db391fba1ab07d88e0bfd
HatsuSixty <hatsunotsus@gmail.com>
2023-09-18 21:57:52 -0300

    Refactor build script to eliminate code duplication

--- build.sh
+++ build.sh
@@ -2,73 +2,105 @@
 
 set -e
 
+log() {
+    level=$1
+    message=$2
+    case $level in
+        1)
+            printf "\n --- $message --- \n\n"
+            ;;
+        15)
+            printf "\n --- $message --- \n"
+            ;;
+        2)
+            printf "\n$message\n\n"
+            ;;
+        3)
+            printf " >>> $message\n"
+            ;;
+        4)
+            printf "   -> $message\n"
+            ;;
+        *)
+            echo $message
+            ;;
+    esac
+}
+
 unzip() {
-    echo "Unzipping \`$1\`"
+    log 5 "Unzipping \`$1\`"
     /bin/unzip -qq $@
 }
 
+if [ ! -d .build_cache ]; then
+    log 5 "[INFO] Creating \`.build_cache\`"
+    mkdir .build_cache
+fi
+
 if [ ! -d Build ]; then
-    printf "\n --- Downloading MineClone2 --- \n\n"
-    if [ ! -f mineclone2.zip ]; then
-        wget https://git.minetest.land/MineClone2/MineClone2/archive/b4c693bb205777b39446fe631ac02823656cc26d.zip -O mineclone2.zip
+    log 1 "Downloading MineClone2"
+    if [ ! -f ./.build_cache/mineclone2.zip ]; then
+        wget https://git.minetest.land/MineClone2/MineClone2/archive/b4c693bb205777b39446fe631ac02823656cc26d.zip -O ./.build_cache/mineclone2.zip
     fi
-    unzip mineclone2.zip
+    unzip ./.build_cache/mineclone2.zip
     mv mineclone2 Build
 fi
 
-printf "\n --- Patching menu --- \n\n"
-rm -vf ./Build/menu/header*.png
-cp -v ./Assets/header.png ./Build/menu/header.png
-rm -vf ./Build/menu/icon*.png
-cp -v ./Assets/icon.png ./Build/menu/icon.png
+log 1 "Patching assets"
 
-printf "\n --- Patching game metadata --- \n\n"
-rm -vf ./Build/game.conf
-cp -vf ./game.conf ./Build/game.conf
+log 3 "Patching menu"
+rm -f ./Build/menu/header*.png
+cp ./Assets/header.png ./Build/menu/header.png
+rm -f ./Build/menu/icon*.png
+cp ./Assets/icon.png ./Build/menu/icon.png
 
-printf "\n --- Patching Amethyst sounds --- \n\n"
-rm -vf ./Build/mods/ITEMS/mcl_amethyst/sounds/*
-cp -vf ./Assets/Sounds/mcl_amethyst_amethyst* ./Build/mods/ITEMS/mcl_amethyst/sounds/
+log 3 "Patching game metadata"
+rm -f ./Build/game.conf
+cp -f ./game.conf ./Build/game.conf
 
-printf "\n --- Patching Barrel sounds --- \n\n"
-rm -vf ./Build/mods/ITEMS/mcl_barrels/sounds/*
-cp -vf ./Assets/Sounds/mcl_barrels_default_barrel* ./Build/mods/ITEMS/mcl_barrels/sounds/
+log 3 "Patching Amethyst sounds"
+rm -f ./Build/mods/ITEMS/mcl_amethyst/sounds/*
+cp -f ./Assets/Sounds/mcl_amethyst_amethyst* ./Build/mods/ITEMS/mcl_amethyst/sounds/
 
-printf "\n --- Patching Stone sounds --- \n\n"
-rm -vf ./Build/mods/CORE/mcl_sounds/sounds/default_dig_cracky*
-rm -vf ./Build/mods/CORE/mcl_sounds/sounds/default_hard_footstep*
-cp -vf ./Assets/Sounds/default_* ./Build/mods/CORE/mcl_sounds/sounds/
+log 3 "Patching Barrel sounds"
+rm -f ./Build/mods/ITEMS/mcl_barrels/sounds/*
+cp -f ./Assets/Sounds/mcl_barrels_default_barrel* ./Build/mods/ITEMS/mcl_barrels/sounds/
 
-printf "\n --- Patching Amethyst textures --- \n\n"
-rm -vf ./Build/textures/mcl_amethyst_amethyst_block.png
-rm -vf ./Build/textures/mcl_amethyst_amethyst_cluster.png
-rm -vf ./Build/textures/mcl_amethyst_budding_amethyst.png
-cp -vf ./Assets/Textures/mcl_amethyst_amethyst_block.png ./Build/textures/
-cp -vf ./Assets/Textures/mcl_amethyst_amethyst_cluster.png ./Build/textures/
-cp -vf ./Assets/Textures/mcl_amethyst_budding_amethyst.png ./Build/textures/
+log 3 "Patching Stone sounds"
+rm -f ./Build/mods/CORE/mcl_sounds/sounds/default_dig_cracky*
+rm -f ./Build/mods/CORE/mcl_sounds/sounds/default_hard_footstep*
+cp -f ./Assets/Sounds/default_* ./Build/mods/CORE/mcl_sounds/sounds/
 
-printf "\n --- Patching Ancient Debris textures --- \n\n"
-rm -vf ./Build/textures/mcl_nether_ancient_debris_side.png
-rm -vf ./Build/textures/mcl_nether_ancient_debris_top.png
-cp -vf ./Assets/Textures/mcl_nether_ancient_debris_side.png ./Build/textures/
-cp -vf ./Assets/Textures/mcl_nether_ancient_debris_top.png ./Build/textures/
+log 3 "Patching Amethyst textures"
+rm -f ./Build/textures/mcl_amethyst_amethyst_block.png
+rm -f ./Build/textures/mcl_amethyst_amethyst_cluster.png
+rm -f ./Build/textures/mcl_amethyst_budding_amethyst.png
+cp -f ./Assets/Textures/mcl_amethyst_amethyst_block.png ./Build/textures/
+cp -f ./Assets/Textures/mcl_amethyst_amethyst_cluster.png ./Build/textures/
+cp -f ./Assets/Textures/mcl_amethyst_budding_amethyst.png ./Build/textures/
 
-printf "\n --- Patching Andesite textures --- \n\n"
-rm -vf ./Build/textures/mcl_core_andesite.png
-cp -vf ./Assets/Textures/mcl_core_andesite.png ./Build/textures/
+log 3 "Patching Ancient Debris textures"
+rm -f ./Build/textures/mcl_nether_ancient_debris_side.png
+rm -f ./Build/textures/mcl_nether_ancient_debris_top.png
+cp -f ./Assets/Textures/mcl_nether_ancient_debris_side.png ./Build/textures/
+cp -f ./Assets/Textures/mcl_nether_ancient_debris_top.png ./Build/textures/
 
-printf "\n --- Patching Bedrock textures --- \n\n"
-rm -vf ./Build/textures/mcl_core_bedrock.png
-cp -vf ./Assets/Textures/mcl_core_bedrock.png ./Build/textures/
+log 3 "Patching Andesite textures"
+rm -f ./Build/textures/mcl_core_andesite.png
+cp -f ./Assets/Textures/mcl_core_andesite.png ./Build/textures/
 
-printf "\n --- Patching Diorite textures --- \n\n"
-rm -vf ./Build/textures/mcl_core_diorite.png
-cp -vf ./Assets/Textures/mcl_core_diorite.png ./Build/textures/
+log 3 "Patching Bedrock textures"
+rm -f ./Build/textures/mcl_core_bedrock.png
+cp -f ./Assets/Textures/mcl_core_bedrock.png ./Build/textures/
 
-printf "\n --- Removing ambient music --- \n\n"
-rm -vrf ./Build/mods/PLAYER/mcl_music
+log 3 "Patching Diorite textures"
+rm -f ./Build/textures/mcl_core_diorite.png
+cp -f ./Assets/Textures/mcl_core_diorite.png ./Build/textures/
 
-printf "\n --- Patching source code --- \n\n"
+log 3 "Removing ambient music"
+rm -rf ./Build/mods/PLAYER/mcl_music
+
+log 1 "Patching source code"
 if [ -f Build/patched ]; then
     printf "%s" "WARNING: It looks like you already have a previous Build of OhioCraft. In order to apply patches, you would need to restart the build process. Do you want to skip this step? (y/n) " >&2
     read answer
@@ -84,69 +116,69 @@ else
     patch ./Build/mods/ITEMS/mcl_barrels/init.lua < Patches/mcl_barrels_init_lua.patch
     touch Build/patched
 
-    printf "\n >>> Patching REDSTONE\n\n"
-    echo "  -> Replacing \`mesecon\` with \`mesecon2\`"
+    log 2 "Patching REDSTONE"
+    log 3 "Replacing \`mesecon\` with \`mesecon2\`"
     find ./Build/mods -type f -exec \
          sed -i {} -e 's/mesecon/mesecon2/g' \;
-    echo "  -> Adapting texture names"
+    log 3 "Adapting texture names"
     for f in ./Build/textures/*; do
         if [[ $f == *mesecons* ]]; then
             mv "$f" "${f/mesecons/mesecon2s}"
         fi
     done
-    echo "  -> Adapting sound names"
+    log 3 "Adapting sound names"
     find ./Build/mods/ITEMS/REDSTONE -type f -name "*mesecons*.ogg" -exec \
          bash -c \
          "bn=\$(basename \$0); bd=\$(dirname \$0); mv \$0 \$bd/\${bn/mesecons/mesecon2s}" {} \;
-    echo "  -> Adapting translation files"
+    log 3 "Adapting translation files"
     find ./Build/mods/ITEMS/REDSTONE -type f -name "*mesecons*.tr" -exec \
          bash -c \
          "bn=\$(basename \$0); bd=\$(dirname \$0); mv \$0 \$bd/\${bn/mesecons/mesecon2s}" {} \;
-    echo "  -> Done!"
+    log 3 "Done!"
 fi
 
-printf "\n --- Adding mods --- \n"
+log 15 "Adding mods"
 
-printf "\nAdding OhioCraft specific mods\n\n"
-rm -vrf ./Build/mods/OHIOCRAFT
-cp -vr ./Mods ./Build/mods/OHIOCRAFT
+log 2 "Adding OhioCraft specific mods"
+log 3 "Copying \`./Mods\` to \`./Build/mods/OHIOCRAFT\`"
+rm -rf ./Build/mods/OHIOCRAFT
+cp -r ./Mods ./Build/mods/OHIOCRAFT
 
-printf "\nAdding World Edit\n\n"
-if [ ! -f worldedit.zip ]; then
-    wget https://github.com/Uberi/Minetest-WorldEdit/archive/refs/tags/1.3.zip -O worldedit.zip
-fi
-if [ -d "Minetest-WorldEdit-1.3" ]; then
-    rm -rf "Minetest-WorldEdit-1.3"
-fi
-unzip worldedit.zip
-rm -vrf ./Build/mods/WORLDEDIT
-cp -vr ./Minetest-WorldEdit-1.3 ./Build/mods/WORLDEDIT
+install_mod() {
+    url=$1
+    output_folder=$2
+    file=./.build_cache/$(basename $url)
 
-printf "\n >>> Removing worldedit_gui\n\n"
-rm -vrf ./Build/mods/WORLDEDIT/worldedit_gui
+    if [ ! -f $file ]; then
+        wget $url -O $file
+    fi
+    zip_folder=./.build_cache/`basename $(zipinfo -1 $file | head -1)`
+    if [ -d $zip_folder ]; then
+        rm -rf $zip_folder
+    fi
+    unzip $file -d ./.build_cache
 
-printf "\nAdding mcl_decor\n\n"
-if [ ! -f decor.zip ]; then
-    wget https://codeberg.org/rudzik8/mcl_decor/archive/v1.4_01.zip -O decor.zip
-fi
-if [ -d mcl_decor ]; then
-    rm -rf mcl_decor
-fi
-unzip decor.zip
-rm -vrf ./Build/mods/DECOR
-cp -vr ./mcl_decor ./Build/mods/DECOR
+    rm -rf $output_folder
 
-printf "\nAdding mesecons\n\n"
-if [ ! -f mesecons.zip ]; then
-    wget https://github.com/minetest-mods/mesecons/archive/8e30ee411347e6552cb5a9f28ea3069805853ea3.zip -O mesecons.zip
-fi
-if [ -d mesecons-8e30ee411347e6552cb5a9f28ea3069805853ea3 ]; then
-    rm -rf mesecons-8e30ee411347e6552cb5a9f28ea3069805853ea3
-fi
-unzip mesecons.zip
-rm -vrf ./Build/mods/MESECONS
-cp -vr ./mesecons-8e30ee411347e6552cb5a9f28ea3069805853ea3 \
-   ./Build/mods/MESECONS
+    log 3 "Copying \`$zip_folder\` contents"
+    cp -r $zip_folder $output_folder
+}
 
-printf "\n >>> Removing mesecons_doors\n\n"
-rm -vrf ./Build/mods/MESECONS/mesecons_doors
+log 2 "Adding worldedit"
+install_mod \
+    https://github.com/Uberi/Minetest-WorldEdit/archive/refs/tags/1.3.zip \
+    ./Build/mods/WORLDEDIT
+log 3 "Removing worldedit_gui"
+rm -rf ./Build/mods/WORLDEDIT/worldedit_gui
+
+log 2 "Adding mcl_decor"
+install_mod \
+    https://codeberg.org/rudzik8/mcl_decor/archive/v1.4_01.zip \
+    ./Build/mods/DECOR
+
+log 2 "Adding mesecons"
+install_mod \
+    https://github.com/minetest-mods/mesecons/archive/8e30ee411347e6552cb5a9f28ea3069805853ea3.zip \
+    ./Build/mods/MESECONS
+log 3 "Removing mesecons_doors"
+rm -rf ./Build/mods/MESECONS/mesecons_doors
