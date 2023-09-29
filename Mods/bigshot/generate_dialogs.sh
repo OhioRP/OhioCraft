ae59597198937041ebc2f55ac6b3ae47d101136a
HatsuSixty <hatsunotsus@gmail.com>
2023-09-29 11:20:50 -0300

    Refactor `generate_dialogs.py` script so it can read dialogs from a file

--- Mods/bigshot/generate_dialogs.sh
+++ Mods/bigshot/generate_dialogs.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-./generate_dialog.py \
-    "\"HEY      EVERY      !! IT'S ME!!!\"
-     \"EV3RY  BUDDY  'S FAVORITE [[Number 1 Rated Salesman1997]]\"
-     \"SPAMTON G. SPAMTON!!\"" spamton > spamton_dialog.lua
+./generate_dialogs.py -i spamton_dialog.txt -o spamton_dialog.lua spamton
--- Mods/bigshot/spamton_dialog.lua
+++ Mods/bigshot/spamton_dialog.lua
@@ -19,13 +19,148 @@ minetest.handle_async(function()
 local t0 = os.clock()
 while os.clock() - t0 <= 5 do end
 end, function()
-minetest.chat_send_player(player, "SPAMTON G. SPAMTON!!")
+minetest.chat_send_player(player, 'SPAMTON G. SPAMTON!!')
+minetest.handle_async(function()
+local t0 = os.clock()
+while os.clock() - t0 <= 5 do end
+end, function()
+minetest.chat_send_player(player, 'WOAH!! IF IT ISN"T A...')
+minetest.handle_async(function()
+local t0 = os.clock()
+while os.clock() - t0 <= 5 do end
+end, function()
+minetest.chat_send_player(player, 'LIGHT nER! HEY-HE Y HEY!!!')
+minetest.handle_async(function()
+local t0 = os.clock()
+while os.clock() - t0 <= 5 do end
+end, function()
+minetest.chat_send_player(player, "LOOKS LIKE YOU'RE [[All Alone On A Late Night?]]")
+minetest.handle_async(function()
+local t0 = os.clock()
+while os.clock() - t0 <= 5 do end
+end, function()
+minetest.chat_send_player(player, 'ALL YOUR FRIENDS, [[Abandoned you for the slime]] YOU ARE?')
+minetest.handle_async(function()
+local t0 = os.clock()
+while os.clock() - t0 <= 5 do end
+end, function()
+minetest.chat_send_player(player, 'SALES, GONE DOWN THE [[Drain]] [[Drain]]??')
+minetest.handle_async(function()
+local t0 = os.clock()
+while os.clock() - t0 <= 5 do end
+end, function()
+minetest.chat_send_player(player, 'LIVING IN A GODDAMN GARBAGE CAN???')
+minetest.handle_async(function()
+local t0 = os.clock()
+while os.clock() - t0 <= 5 do end
+end, function()
+minetest.chat_send_player(player, 'WELL HAVE I GOT A [[Specil Deal]] FOR LONELY [[Hearts]] LIKE YOU!!')
+minetest.handle_async(function()
+local t0 = os.clock()
+while os.clock() - t0 <= 5 do end
+end, function()
+minetest.chat_send_player(player, "IF YOU'VE [[Lost Control Of Your Life]]")
+minetest.handle_async(function()
+local t0 = os.clock()
+while os.clock() - t0 <= 5 do end
+end, function()
+minetest.chat_send_player(player, 'THEN YOU JUST GOTTA GRAB IT BY THE [[Silly Strings]]')
+minetest.handle_async(function()
+local t0 = os.clock()
+while os.clock() - t0 <= 5 do end
+end, function()
+minetest.chat_send_player(player, 'WHY BE THE [[Little Sponge]] WHO HATES ITS [[$4.99]] LIFE')
+minetest.handle_async(function()
+local t0 = os.clock()
+while os.clock() - t0 <= 5 do end
+end, function()
+minetest.chat_send_player(player, 'WHEN YOU CAN BE A')
+minetest.handle_async(function()
+local t0 = os.clock()
+while os.clock() - t0 <= 5 do end
+end, function()
+minetest.chat_send_player(player, '[[BIG SHOT!!!]]')
+minetest.handle_async(function()
+local t0 = os.clock()
+while os.clock() - t0 <= 5 do end
+end, function()
+minetest.chat_send_player(player, '[[BIG SHOT!!!!]]')
+minetest.handle_async(function()
+local t0 = os.clock()
+while os.clock() - t0 <= 5 do end
+end, function()
+minetest.chat_send_player(player, '[[BIG SHOT!!!!!]]')
+minetest.handle_async(function()
+local t0 = os.clock()
+while os.clock() - t0 <= 5 do end
+end, function()
+minetest.chat_send_player(player, "THAT'S RIGHT!! NOW'S YOUR CHANCE TO BE A [[BIG SHOT]]!!")
+minetest.handle_async(function()
+local t0 = os.clock()
+while os.clock() - t0 <= 5 do end
+end, function()
+minetest.chat_send_player(player, 'AND I HAVE JUST.')
+minetest.handle_async(function()
+local t0 = os.clock()
+while os.clock() - t0 <= 5 do end
+end, function()
+minetest.chat_send_player(player, 'THE THING.')
+minetest.handle_async(function()
+local t0 = os.clock()
+while os.clock() - t0 <= 5 do end
+end, function()
+minetest.chat_send_player(player, 'YOU NEED.')
+minetest.handle_async(function()
+local t0 = os.clock()
+while os.clock() - t0 <= 5 do end
+end, function()
+minetest.chat_send_player(player, "THAT'S")
+minetest.handle_async(function()
+local t0 = os.clock()
+while os.clock() - t0 <= 5 do end
+end, function()
+minetest.chat_send_player(player, '[[Hyperlink Blocked]].')
+minetest.handle_async(function()
+local t0 = os.clock()
+while os.clock() - t0 <= 5 do end
+end, function()
+minetest.chat_send_player(player, "YOU WANT IT. YOU WANT [[Hyperlink Blocked]], DON'T YOU.")
+minetest.handle_async(function()
+local t0 = os.clock()
+while os.clock() - t0 <= 5 do end
+end, function()
+minetest.chat_send_player(player, 'WELL HAVE I GOT A DEAL FOR YOU!!')
+minetest.handle_async(function()
+local t0 = os.clock()
+while os.clock() - t0 <= 5 do end
+end, function()
+minetest.chat_send_player(player, 'ALL YOU HAVE TO DO IS SHOW ME.')
+minetest.handle_async(function()
+local t0 = os.clock()
+while os.clock() - t0 <= 5 do end
+end, function()
+minetest.chat_send_player(player, 'YOUR [[HeartShapedObject]].')
+minetest.handle_async(function()
+local t0 = os.clock()
+while os.clock() - t0 <= 5 do end
+end, function()
+minetest.chat_send_player(player, "YOU'RE  LIGHT neR< AREN'T YOU?")
+minetest.handle_async(function()
+local t0 = os.clock()
+while os.clock() - t0 <= 5 do end
+end, function()
+minetest.chat_send_player(player, "YOU'VE GOT THE [[LIGHT.]] WHY DON'T YOU [[Show it off?]]")
+minetest.handle_async(function()
+local t0 = os.clock()
+while os.clock() - t0 <= 5 do end
+end, function()
+minetest.chat_send_player(player, 'HAEAHAEAHAEAHAEAH!!')
 if theme_song then
 minetest.sound_stop(handle)
 end
 if callback then
 callback()
 end
-end)end)end)end
+end)end)end)end)end)end)end)end)end)end)end)end)end)end)end)end)end)end)end)end)end)end)end)end)end)end)end)end)end)end)end
 
 return spamton_dialog
