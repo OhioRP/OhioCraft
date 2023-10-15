8eb46729609a6eecc38da50a0afda564326b35b6
HatsuSixty <hatsunotsus@gmail.com>
2023-10-15 13:26:30 -0300

    Add mini-spamtons
    They'll be used for Spamton's battle in the future.

--- Mods/bigshot/battle.lua
+++ Mods/bigshot/battle.lua
@@ -1,5 +1,34 @@
 local battle = {}
 
+mcl_mobs.register_mob("bigshot:minispamton", {
+    type = "monster",
+    visual = "mesh",
+    mesh = "bigshot_block_spamton.obj",
+    textures = { "bigshot_entity_minispamton.png" },
+    texture_mods = "",
+    collisionbox = { -0.2, -0.5, -0.2, 0.2, 0.2, 0.2 },
+    fear_height = 4,
+    hp_min = 100,
+    hp_max = 100,
+    breath_max = -1,
+    armor = { undead = 100, fleshy = 100 },
+    pathfinding = 1,
+    group_attack = true,
+    visual_size = { x = 0.9, y = 0.9 },
+    makes_footstep_sound = true,
+    sounds = {
+        random = "spamton-laugh-noise",
+        death = "spamton-laugh-noise",
+        damage = "spamton-laugh-noise",
+        distance = 16,
+    },
+    damage = 7,
+    reach = 2,
+    attack_type = "dogfight",
+    view_range = 16
+})
+mcl_mobs:non_spawn_specific("mobs_mc:endermite", "overworld", 0, 7)
+
 function battle.start_battle(pn)
     print(pn)
 
