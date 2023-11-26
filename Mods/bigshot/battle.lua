a8b036ab7d838904d75ed23d8efc6d66e0702f6d
HatsuSixty <hatsunotsus@gmail.com>
2023-11-25 21:37:11 -0300

    Actually implement Spamton's battle

--- Mods/bigshot/battle.lua
+++ Mods/bigshot/battle.lua
@@ -1,3 +1,6 @@
+local modname = minetest.get_current_modname()
+local S = minetest.get_translator(modname)
+
 local battle = {}
 
 mcl_mobs.register_mob("bigshot:minispamton", {
@@ -27,21 +30,114 @@ mcl_mobs.register_mob("bigshot:minispamton", {
     attack_type = "dogfight",
     view_range = 16
 })
-mcl_mobs:non_spawn_specific("mobs_mc:endermite", "overworld", 0, 7)
+mcl_mobs:non_spawn_specific("bigshot:minispamton", "overworld", 0, 7)
+
+local function spawn_minispamtons(pn)
+    minetest.sound_play({ name = "spamton-laugh-noise" })
+    local player = minetest.get_player_by_name(pn)
+    for _ = 0, 10, 1 do
+        local pos = player:get_pos()
+        pos.x = pos.x + 10
+        pos.z = pos.z + 10
+        minetest.add_entity(pos, "bigshot:minispamton")
+    end
+end
+
+local function lightning_strikes(pn)
+    local player = minetest.get_player_by_name(pn)
+
+    local idx = 0
+    local i = 0
+    local texts = {"NOW'S YOUR CHANCE",
+                   "TO BE A", "[[Big Shot]]"}
+    local texts_index = 1
+    local function loop()
+        if i >= 6 then
+            player:hud_remove(idx)
+            return
+        end
+
+        player:hud_remove(idx)
+        idx = player:hud_add({
+             hud_elem_type = "text",
+             position      = {x = 0.5, y = 0.5},
+             offset        = {x = 0,   y = 0},
+             text          = texts[texts_index],
+             alignment     = {x = 0, y = 0},
+             scale         = {x = 1, y = 1},
+             number        = 0xFFFFFF,
+        })
+        texts_index = texts_index + 1
+        if texts_index > #texts then
+            texts_index = 1
+        end
+        lightning.strike(player:get_pos())
+
+        i = i + 1
+        minetest.after(1, loop)
+    end
+    loop()
+end
+
+local function transfer_kromer(pn)
+    local player = minetest.get_player_by_name(pn)
+
+    local inv = player:get_inventory()
+    local chosen_item = nil
+    local taken = nil
+    if not inv:is_empty("main") then
+        repeat
+            chosen_item = inv:get_stack("main", math.random(inv:get_size("main")))
+            taken = inv:remove_item("main", chosen_item)
+        until not taken:is_empty()
+    end
+
+    local idx = player:hud_add({
+         hud_elem_type = "text",
+         position      = {x = 0.5, y = 0.5},
+         offset        = {x = 0,   y = 0},
+         text          = S("TRANSFERING [[Kromer]]"),
+         alignment     = {x = 0, y = 0},
+         scale         = {x = 1, y = 1},
+         number        = 0xFFFFFF,
+    })
+
+    minetest.after(18, function()
+        player:hud_remove(idx)
+        if taken == nil or chosen_item == nil then
+            minetest.chat_send_player(pn, "NO [[Kromer]] TRANSFERED :(")
+        else
+            minetest.chat_send_player(pn, S("REMOVED @1 `@2`s FROM YOUR INVENTORY LMAO",
+                                            taken:get_count(), chosen_item:get_name()))
+        end
+    end)
+end
 
 function battle.start_battle(pn)
-    print(pn)
+    local functions = {transfer_kromer, spawn_minispamtons, lightning_strikes}
+    local functions_index = 1
 
     local handle = minetest.sound_play({
         name = "now-s-your-chance-to-be-a"
     }, { loop = true })
 
-    minetest.handle_async(function()
-        local t0 = os.clock()
-        while os.clock() - t0 <= 60 do end
-    end, function()
-        minetest.sound_stop(handle)
-    end)
+    local i = 0
+    local function loop()
+        if i >= 10 then
+            minetest.sound_stop(handle)
+            return
+        end
+
+        (functions[functions_index])(pn)
+        functions_index = functions_index + 1
+        if functions_index > #functions then
+            functions_index = 1
+        end
+
+        i = i + 1
+        minetest.after(20, loop)
+    end
+    loop()
 end
 
 return battle
